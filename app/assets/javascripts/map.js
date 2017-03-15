b20e9171228d1410fffae8abe24b8ad43e6ce4f1
Jesse Novotny <jessenovotny@gmail.com>
2017-03-14 21:07:14 -0700

    added css, html, and route for contact page

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function initMap(){
+  var get_latitude = $('#google-map').data('latitude');
+  var get_longitude = $('#google-map').data('longitude');
+
+  var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+  var mapOptions = {
+      zoom: 12,
+      scrollwheel: false,
+      center: myLatlng,
+      disableDefaultUI: true,
+      gestureHandling: 'none'
+  };
+  var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+}
