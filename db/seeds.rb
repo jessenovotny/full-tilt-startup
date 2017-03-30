101566fa5e19d9dc4a01bed62480e793d7295212
Jesse Novotny <jessenovotny@gmail.com>
2017-03-29 22:07:11 -0700

    updated seeds and custom.css

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,25 +52,18 @@ real_testimonials = [
 		content: "Jesse was a foundational element to our team at Student-Tutor. From his first day, he went above and beyond the call of duty, eager to take on more challenges.</p><p>Not only was he an exceptional sales associate, but he was also a major contributor to our website UX, sales funnel structure, and completely automated our daily operations in Infusionsoft.</p><p>Rather than call Infusionsoft for technical support, Jesse quickly became our go-to problem solver."
 	},
 	{
-		name: "Beke Richardson",
-		position: "Real Estate Agent",
-		company: "Ray White Barrington",
-		image_url: "#{aws_path}/beke-r.jpg",
-		content: "After the devastating 2011 earthquake in Christchurch New Zealand, Greenpeace had to cease all fundraising efforts in the area. As the Team Leader at the time, I was relocated to Auckland and was eager to return home. After a few months, Jesse was hired and offered to help me pilot a program to test the areas fundraising viability.</p><p>Thanks to Jesse's assistance and unwavering commitment, I was able to establish an extremely successful team to once again campaign for Greenpeace in Christchurch NZ."
-	},
-	{
 		name: "Andrea Blake", 
 		position: "Travel Blogger",
 		company: "Southwest Sampler",
 		image_url: "#{aws_path}/andrea-b.jpg",
-		content: "Jesse is phenomenal at what he does - he is very knowledgeable in both web development and digital marketing. Never have I had anyone go into as much detail as he did for me in the one hour I spent with him. Organized, efficient and over all crystal clear notes. I now feel more prepared than ever, thanks to Jesse."
+		content: "Jesse is phenomenal at what he does - he is very knowledgeable in both web development and digital marketing.</p><p>Never have I had anyone go into as much detail as he did for me in the one hour I spent with him. Organized, efficient and over all crystal clear notes. I now feel more prepared than ever, thanks to Jesse."
 	},
 	{
 		name: "Arash Tadayon", 
 		position: "CEO",
 		company: "Dezapp",
 		image_url: "#{aws_path}/arash-t.jpg",
-		content: "I've seen Jesse's progress in web development skyrocket since back when he could barely read a single page of basic css. He's a guy I've always had a pleasure talking shop with and from my experience, he's got the knowledge and tools to accomplish anything he puts his mind to. I appreciate that he comprehends the technical aspects of a project as well as the importance of UX from a digital marketing standpoint. As for his code, I can tell you it looks good."
+		content: "I've seen Jesse's progress in web development skyrocket since back when he could barely read a single page of basic css.</p><p>He's a guy I've always had a pleasure talking shop with and from my experience, he's got the knowledge and tools to accomplish anything he puts his mind to.</p><p>I appreciate that he comprehends the technical aspects of a project as well as the importance of UX from a digital marketing standpoint. As for his code, I can tell you it looks good."
 	},
 	{
 		name: "Tony Caldwell", 
@@ -78,6 +71,13 @@ real_testimonials = [
 		company: "Pacific Change Semiconductors",
 		image_url: "#{aws_path}/tony-c.jpg", 
 		content: "Jesse is a humble leader and outstanding businessman. He taught me a great deal about organizational efficiency and business development. If you need someone with skill sets far beyond what he may initially reveal, choose Jesse."
+	},
+	{
+		name: "Beke Richardson",
+		position: "Real Estate Agent",
+		company: "Ray White Barrington",
+		image_url: "#{aws_path}/beke-r.jpg",
+		content: "After the devastating 2011 earthquake in Christchurch New Zealand, Greenpeace had to cease all fundraising efforts in the area. As the Team Leader at the time, I was relocated to Auckland and was eager to return home. After a few months, Jesse was hired and offered to help me pilot a program to test the areas fundraising viability.</p><p>Thanks to Jesse's assistance and unwavering commitment, I was able to establish an extremely successful team to once again campaign for Greenpeace in Christchurch NZ."
 	},
 	{
 		name: "Alix Prappas", 
